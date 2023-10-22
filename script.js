e86f27b8cc0817ba691c9df750dfeb94380cebeb
neidhwp <145912807+neidhwp@users.noreply.github.com>
2023-10-22 19:34:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let links = document.querySelectorAll('.scroll');
+let targetID;
+links.forEach(function (element){
+    element.addEventListener('click', function (event){
+        event.preventDefault();
+        targetID = element.getAttribute('href');
+        document.querySelector(targetID).scrollIntoView({
+            behavior: "smooth",
+            block: 'start'
+        })
+    })
+})
+
+$(document).ready(function () {
+    $('.slider').bxSlider();
+});
